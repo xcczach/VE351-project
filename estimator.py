4fbaf2734958786e3afedcd81c3e17fdbd1bfa44
xcc <2867389537@qq.com>
2024-11-28 22:43:19 +0800

    better test file

--- estimator.py
+++ estimator.py
@@ -4,11 +4,18 @@ from abc import ABC, abstractmethod
 
 class Estimator(ABC):
     @abstractmethod
+    def label(self):
+        pass
+
+    @abstractmethod
     def estimate_channel(self, Y_k: np.ndarray, X_k: np.ndarray) -> np.ndarray:
         pass
 
 
 class DirectRatioEstimator(Estimator):
+    def label(self):
+        return "Direct Ratio Estimator"
+
     def estimate_channel(self, Y_k: np.ndarray, X_k: np.ndarray) -> np.ndarray:
-        ZERO_THRESHOLD = 1e-20
+        ZERO_THRESHOLD = 1e-12
         return np.where(X_k < ZERO_THRESHOLD, 0, Y_k / X_k)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from simulator import Simulator
-from estimator import DirectRatioEstimator
+from estimator import Estimator, DirectRatioEstimator
+from typing import List
 
 if __name__ == "__main__":
     N = 1024
@@ -12,6 +13,7 @@ if __name__ == "__main__":
         [0, 5 * Ts, 12 * Ts, 30 * Ts, 50 * Ts],
         [0, 5.4 * Ts, 12.5 * Ts, 30 * Ts, 50.5 * Ts],
     ]
+    estimators: List[Estimator] = [DirectRatioEstimator()]
     for p in p_arr:
         X_k = simulator.create_pilot_Xk(p)
         for h_t_ind in range(len(h_t_amplitudes)):
@@ -22,8 +24,12 @@ if __name__ == "__main__":
             for SNR in SNR_arr:
                 W_k = simulator.create_gaussian_noise_Wk(SNR, p, H_k, X_k)
                 Y_k = simulator.create_Y_k(H_k, X_k, W_k)
-                estimator = DirectRatioEstimator()
-                H_k_hat = estimator.estimate_channel(Y_k, X_k)
-                mse = simulator.mse(H_k_hat, H_k)
-                print(f"p={p}, SNR={SNR}, mse={mse}")
-                simulator.comparison_freq_mag_plot(H_k, H_k_hat, f"H_k_{p}_{SNR}.png")
+                for estimator in estimators:
+                    H_k_hat = estimator.estimate_channel(Y_k, X_k)
+                    mse = simulator.mse(H_k_hat, H_k)
+                    print(f"estimator={estimator.label()} ,p={p}, SNR={SNR}, mse={mse}")
+                    simulator.comparison_freq_mag_plot(
+                        H_k,
+                        H_k_hat,
+                        f"H_k_{estimator.label()}_{p}_{SNR}_mse{mse.round(1)}.png",
+                    )
